1f2ce45f0e7d9f5329b97bdea6f348bc25534e5d
Lawrence Mitchell <lmitchell@nvidia.com>
2023-11-24 16:22:57 +0000

    Expose cudf::detail::get_distinct_indices as cudf::distinct_indices
    As well as being able to drop duplicates from a table, it is useful to
    be able to mark duplicates in the table. This is the information
    provided by get_distinct_indices, so promote it to a public function.

--- cpp/include/cudf/stream_compaction.hpp
+++ cpp/include/cudf/stream_compaction.hpp
@@ -278,6 +278,28 @@ std::unique_ptr<table> distinct(
   rmm::mr::device_memory_resource* mr = rmm::mr::get_current_device_resource());
 
 /**
+ * @brief Create a column of indices of all distinct rows in the input table.
+ *
+ * Given an `input` table_view, an output vector of all row indices of the distinct rows is
+ * generated. If there are duplicate rows, which index is kept depends on the `keep` parameter.
+ *
+ * @param input The input table
+ * @param keep Get index of any, first, last, or none of the found duplicates
+ * @param nulls_equal Flag to specify whether null elements should be considered as equal
+ * @param nans_equal Flag to specify whether NaN elements should be considered as equal
+ * @param stream CUDA stream used for device memory operations and kernel launches
+ * @param mr Device memory resource used to allocate the returned vector
+ * @return Column containing the result indices
+ */
+std::unique_ptr<column> distinct_indices(
+  table_view const& input,
+  duplicate_keep_option keep          = duplicate_keep_option::KEEP_ANY,
+  null_equality nulls_equal           = null_equality::EQUAL,
+  nan_equality nans_equal             = nan_equality::ALL_EQUAL,
+  rmm::cuda_stream_view stream        = cudf::get_default_stream(),
+  rmm::mr::device_memory_resource* mr = rmm::mr::get_current_device_resource());
+
+/**
  * @brief Create a new table without duplicate rows, preserving input order.
  *
  * Given an `input` table_view, each row is copied to the output table to create a set of distinct
